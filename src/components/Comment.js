6551b2b550102d55433357549eaf09eee36d734d
Harrison.Ding <dinglinbin@gmail.com>
2018-03-25 10:42:16 +0800

    Add select option in render().

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -3,6 +3,13 @@ import {Content} from './Content'
 
 export class Comment extends React.Component {
     render() {
-        return <Content></Content>;
+        return <div>
+            <select>
+                <option value = "1">Tim</option>
+                <option value = "2">John</option>
+                <option value = "3">Hank</option>
+            </select>
+            <Content></Content>
+            </div>;
     }
 }
