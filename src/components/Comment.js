b1c5528ae9ee3e51aee69bd52da20d515328082f
Harrison.Ding <dinglinbin@gmail.com>
2018-03-25 10:42:16 +0800

    Add select option in render(), including assigned with state.

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -2,7 +2,38 @@ import React from 'react'
 import {Content} from './Content'
 
 export class Comment extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            names: ['Tim', 'John', 'Hank']
+        };
+    }
+
+    /*
+    // It seems browser cannot get state in this function, raising 
+        'names' is null exception
+     getInitialState() {
+        return {
+            names: ['Time', 'John', 'Hank']
+        };
+    } */
+
     render() {
-        return <Content></Content>;
+        let options = [];
+        for (let opt in this.state.names) {
+            options.push(<option value={opt} key={opt}>{this.state.names[opt]}</option>)
+        };
+
+        for(let it in options) {
+            //console.log("it: " + it + ", item: " + JSON.stringify(options[it]));
+            console.dir(options[it]);
+        }
+
+        return <div>
+            <select>
+                {options}
+            </select>
+            <Content></Content>
+            </div>;
     }
 }
